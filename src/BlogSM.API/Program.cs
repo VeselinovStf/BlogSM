c1ea021f11934f84c25176aded32b72176e2fc14
Doni3 <veselinovStf@gmail.com>
2025-03-08 11:41:02 +0200

    Adding API Controllers setup code

--- src/BlogSM.API/Program.cs
+++ src/BlogSM.API/Program.cs
@@ -1,22 +1,29 @@
 using Asp.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllers();
 
- builder.Services.AddApiVersioning(options =>
-   {
-       options.DefaultApiVersion = new ApiVersion(1, 0);
-       options.AssumeDefaultVersionWhenUnspecified = true;
-       options.ReportApiVersions = true;
-       options.ApiVersionReader = ApiVersionReader.Combine(
-           new UrlSegmentApiVersionReader(),
-           new HeaderApiVersionReader("X-Api-Version")
-       );
-   }).AddApiExplorer(options =>
-   {
-       options.GroupNameFormat = "'v'VVV";
-       options.SubstituteApiVersionInUrl = true;
-   });
+    builder.Services.AddApiVersioning(options =>
+      {
+          options.DefaultApiVersion = new ApiVersion(1, 0);
+          options.AssumeDefaultVersionWhenUnspecified = true;
+          options.ReportApiVersions = true;
+          options.ApiVersionReader = ApiVersionReader.Combine(
+              new UrlSegmentApiVersionReader(),
+              new HeaderApiVersionReader("X-Api-Version")
+          );
+      }).AddApiExplorer(options =>
+      {
+          options.GroupNameFormat = "'v'VVV";
+          options.SubstituteApiVersionInUrl = true;
+      });
+}
 
 
 var app = builder.Build();
+{
+    app.MapControllers();
+}
+
 app.Run();
